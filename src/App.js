9a39d29268ed2bfa9c1f20e1c25313dfddc37665
yogiwisesa <yogiwisesa@gmail.com>
2019-03-25 09:19:50 +0700

    node sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { DatePicker, Button } from 'antd';
 import 'antd/dist/antd.css';
 class App extends Component {
